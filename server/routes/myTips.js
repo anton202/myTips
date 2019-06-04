483338bd46db90f18bcee0cf778159b67ec89f15
anton202 <a.kluge202@gmail.com>
2019-06-04 10:34:15 +0300

    Update myTips.js

--- server/routes/myTips.js
+++ server/routes/myTips.js
@@ -5,7 +5,6 @@ const waitrData = require('../models/myTips');
 router.delete('/deleteTip/:id',(req,res)=>{
     waitrData.deleteOne({_id: req.params.id})
     .then(query => {
-        console.log(query);
         res.status(200).json({message:'tip deleted'})
     })
     .catch(error => res.status(500).json({message:'something went wrong,please try again later'}))
