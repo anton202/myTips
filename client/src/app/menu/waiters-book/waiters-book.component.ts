c7681c75710943b1e86971f3f01edbacb0b2e78a
anton202 <a.kluge202@gmail.com>
2019-10-09 18:18:31 +0300

    Revactored css

--- client/src/app/menu/waiters-book/waiters-book.component.ts
+++ client/src/app/menu/waiters-book/waiters-book.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material";
+import { HttpClient } from "@angular/common/http";
 
-import { ConfirmationDialog } from '../../material/confirmationDailog/confirmationDialog.component';
-import { ErrorMessageComponenet } from '../../material/errorMessage/errorMessage.component';
-import { WaitrsBookService } from './waiters-book.service';
-import { Waitr } from './waitrObj';
-import { SignInComponent } from '../../auth/sign-in/sign-in.component';
-import { Store } from '@ngrx/store';
+import { ConfirmationDialog } from "../../material/confirmationDailog/confirmationDialog.component";
+import { ErrorMessageComponenet } from "../../material/errorMessage/errorMessage.component";
+import { WaitrsBookService } from "./waiters-book.service";
+import { Waitr } from "./waitrObj";
+import { SignInComponent } from "../../auth/sign-in/sign-in.component";
+import { Store } from "@ngrx/store";
 
 @Component({
-  selector: 'app-waiters-book',
-  templateUrl: './waiters-book.component.html',
-  styleUrls: ['./waiters-book.component.scss'],
+  selector: "app-waiters-book",
+  templateUrl: "./waiters-book.component.html",
+  styleUrls: ["./waiters-book.component.scss"],
   providers: [WaitrsBookService]
 })
 export class WaitersBookComponent implements OnInit {
@@ -24,70 +24,88 @@ export class WaitersBookComponent implements OnInit {
   public isWaitrAdded: boolean = false;
   public calculatingTips: boolean = false;
   public dataSource: Array<object>;
-  public displayedColumns: Array<string> = ['סה"כ מזומן', 'הפרשה למעסיק', 'עד שעה', 'משעה', 'שם']
+  public displayedColumns: Array<string> = [
+    'סה"כ מזומן',
+    "הפרשה למעסיק",
+    "עד שעה",
+    "משעה",
+    "שם"
+  ];
   public barManTip: number;
   public moneyToEmployer: number;
-  public tipPerHour: number
+  public tipPerHour: number;
   public isTipsCalculated: boolean = false;
   public recalculatingTips: boolean = false;
   public isUserLogdIn: string;
+  totalAmount: FormGroup;
+  waitrShift: FormGroup;
 
-  constructor(private waitrsBookService: WaitrsBookService, private http: HttpClient, public dialog: MatDialog, private store: Store<{auth:{user}}>) { }
+  constructor(
+    private waitrsBookService: WaitrsBookService,
+    private http: HttpClient,
+    public dialog: MatDialog,
+    private store: Store<{ auth: { user } }>
+  ) {}
 
   ngOnInit() {
     this.initializeForms();
-    this.store.select('auth')
-      .subscribe(user =>{
-        this.isUserLogdIn = user.user;
-      })
+    this.store.select("auth").subscribe(user => {
+      this.isUserLogdIn = user.user;
+    });
   }
 
   private getWaitrsNames(): void {
-    this.waitrsBookService.getWorkersNames()
+    this.waitrsBookService
+      .getWorkersNames()
       .subscribe(
-        workersNames => this.workersNames = workersNames,
-        error => this.handelError('משהו השתבש כשניסינו לדלות את השמות של המלצרים מהשרת, נסה שוב.')
-      )
+        workersNames => (this.workersNames = workersNames),
+        error =>
+          this.handelError(
+            "משהו השתבש כשניסינו לדלות את השמות של המלצרים מהשרת, נסה שוב."
+          )
+      );
   }
 
   private initializeForms(): void {
-    this.calculateTipsForm = new FormGroup({
-      totalTips: new FormControl(null, Validators.required),
-      waitrsShift: new FormGroup({
-        waitrsName: new FormControl(null, Validators.required),
-        shiftStartTime: new FormControl(null, Validators.required),
-        shiftEndTime: new FormControl(null, Validators.required)
-      })
-    })
+    this.totalAmount = new FormGroup({
+      totalTips: new FormControl(null, Validators.required)
+    });
+
+    this.waitrShift = new FormGroup({
+      waitrName: new FormControl(null, Validators.required),
+      shiftStartTime: new FormControl(null, Validators.required),
+      shiftEndTime: new FormControl(null, Validators.required)
+    });
   }
 
   public addWaitr(): void {
-    const waitrsShistFG = this.calculateTipsForm.get('waitrsShift');
-    const waitrName = waitrsShistFG.value.waitrsName;
-    const shiftStartTime = waitrsShistFG.value.shiftStartTime;
-    const shiftEndTime = waitrsShistFG.value.shiftEndTime;
+    const waitrName = this.waitrShift.value.waitrName;
+    const shiftStartTime = this.waitrShift.value.shiftStartTime;
+    const shiftEndTime = this.waitrShift.value.shiftEndTime;
 
-    if (this.calculateTipsForm.get('waitrsShift').valid) {
+    if (this.waitrShift.valid) {
       this.waitrsStack.push(new Waitr(waitrName, shiftStartTime, shiftEndTime));
       this.isShiftTimeNotSet = false;
       this.isWaitrAdded = true;
-      waitrsShistFG.get('waitrsName').reset();
+      this.waitrShift.get('waitrName').reset();
     }
 
-    if (!waitrsShistFG.get('shiftStartTime').valid || !waitrsShistFG.get('shiftEndTime').valid) {
+    if (
+      !this.waitrShift.get("shiftStartTime").valid ||
+      !this.waitrShift.get("shiftEndTime").valid
+    ) {
       this.isShiftTimeNotSet = true;
       this.isWaitrAdded = false;
     }
-
   }
 
   public confirmDeleteWaitr(waitrData) {
     const dialogRef = this.dialog.open(ConfirmationDialog);
     dialogRef.afterClosed().subscribe(isActionConfirmed => {
       if (isActionConfirmed) {
-        this.deleteWaitr(waitrData)
+        this.deleteWaitr(waitrData);
       }
-    })
+    });
   }
 
   private deleteWaitr(waitrToDelete): void {
@@ -95,12 +113,12 @@ export class WaitersBookComponent implements OnInit {
       if (waitr.name === waitrToDelete.name) {
         this.waitrsStack.splice(index, 1);
       }
-    })
+    });
 
     //recalculate tips && display spinner and "recalculating tips" text while recalculating.
     this.calculatingTips = true;
     this.isTipsCalculated = false;
-    this.recalculatingTips = true
+    this.recalculatingTips = true;
     setTimeout(() => {
       this.calculateTips();
       // rerender tips table
@@ -112,36 +130,47 @@ export class WaitersBookComponent implements OnInit {
       }
       this.calculatingTips = false;
       this.recalculatingTips = false;
-    }, 2000)
+    }, 2000);
   }
 
   public calculateTips(): void {
-    const totalTip = this.calculateTipsForm.value.totalTips;
+    const totalTip = this.totalAmount.value.totalTips;
     const waitrsStackClone = this.waitrsStack.slice();
     let totalTipMutated = totalTip;
 
     // calculating each waitr total time that he worked,
-    // and then calculating the total time. 
+    // and then calculating the total time.
     this.waitrsBookService.setwaitrsTotalTime(waitrsStackClone);
-    const totalTime = this.waitrsBookService.calculateTotalTime(waitrsStackClone);
+    const totalTime = this.waitrsBookService.calculateTotalTime(
+      waitrsStackClone
+    );
 
     // subtructing from total tip - barMan tip and moneyToEmployer.
-    totalTipMutated -= this.waitrsBookService.barManTip(+totalTip, 10)
-    totalTipMutated -= this.waitrsBookService.moneyToEmployer(totalTime, 6)
+    totalTipMutated -= this.waitrsBookService.barManTip(+totalTip, 10);
+    totalTipMutated -= this.waitrsBookService.moneyToEmployer(totalTime, 6);
 
     //setting barManTip and moneyToEmployer to show to the user.
     this.barManTip = this.waitrsBookService.barManTip(+totalTip, 10);
-    this.moneyToEmployer = Math.floor(this.waitrsBookService.moneyToEmployer(totalTime, 6));
+    this.moneyToEmployer = Math.floor(
+      this.waitrsBookService.moneyToEmployer(totalTime, 6)
+    );
 
     //set all waitrs per hour tip.
-    this.waitrsBookService.setWaitrsPerHourTip(waitrsStackClone, totalTime, totalTipMutated);
-    this.tipPerHour = this.waitrsBookService.tipPerHour(totalTipMutated, totalTime);
+    this.waitrsBookService.setWaitrsPerHourTip(
+      waitrsStackClone,
+      totalTime,
+      totalTipMutated
+    );
+    this.tipPerHour = this.waitrsBookService.tipPerHour(
+      totalTipMutated,
+      totalTime
+    );
 
     //set all waitrs total tip.
     this.waitrsBookService.setWaitrsTotalTip(waitrsStackClone);
 
     //set all waitrs money to employer.
-    this.waitrsBookService.setWaitrsMoneyToEmployer(waitrsStackClone)
+    this.waitrsBookService.setWaitrsMoneyToEmployer(waitrsStackClone);
 
     //rendering the result to the table.
     this.dataSource = waitrsStackClone;
@@ -154,25 +183,29 @@ export class WaitersBookComponent implements OnInit {
   }
 
   private saveCalculatedTips(waitrsStack): void {
-    this.waitrsBookService.sendDataToServer(waitrsStack)
+    this.waitrsBookService
+      .sendDataToServer(waitrsStack)
       .subscribe(
-        () => { },
-        error => this.handelError('משהו השתבש כשניסינו לשמור את הטיפים על השרת, נסה שוב.')
-      )
+        () => {},
+        error =>
+          this.handelError(
+            "משהו השתבש כשניסינו לשמור את הטיפים על השרת, נסה שוב."
+          )
+      );
   }
 
   public isUserLogedIn(): void {
     if (!this.isUserLogdIn) {
-     this.dialog.open(SignInComponent);
-    }else{
+      this.dialog.open(SignInComponent);
+    } else {
       this.getWaitrsNames();
     }
-    }
-  
-    private handelError(errorMessage: string): void{
-      this.dialog.open(ErrorMessageComponenet, {
-        width: '300px',
-        data: errorMessage
-      })
-    }
+  }
+
+  private handelError(errorMessage: string): void {
+    this.dialog.open(ErrorMessageComponenet, {
+      width: "300px",
+      data: errorMessage
+    });
+  }
 }
