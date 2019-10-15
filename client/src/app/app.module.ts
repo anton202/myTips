7464279a616c5f7b7614e54c2956756518b5e2fa
anton202 <a.kluge202@gmail.com>
2019-10-15 12:40:34 +0300

    finished refactoring waitrs-book component css

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,37 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatRippleModule} from '@angular/material/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StoreModule } from "@ngrx/store";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { NgxMaterialTimepickerModule } from "ngx-material-timepicker";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatListModule } from "@angular/material/list";
+import { MatTableModule } from "@angular/material/table";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatRippleModule } from "@angular/material/core";
 
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { MenuComponent } from './menu/menu.component';
-import { WaitersBookComponent } from './menu/waiters-book/waiters-book.component';
-import { ErrorMessageComponenet } from './material/errorMessage/errorMessage.component'
-import { ConfirmationDialog } from './material/confirmationDailog/confirmationDialog.component';
-import { StatisticsComponent } from './menu/statistics/statistics.component';
-import { AppRoutingModule } from './app-routing.module';
-import { Auth } from './auth/auth.service';
-import { AuthInterceptor } from './auth.interceptor';
-import { SettingsComponent } from './settings/settings.component';
-import { NavComponent } from './nav/nav.component';
-import { authReducer } from './auth/store/auth.reducer';
-import { UserAreaComponent } from './user-area/user-area.component';
+import { AppComponent } from "./app.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { SignInComponent } from "./auth/sign-in/sign-in.component";
+import { MenuComponent } from "./menu/menu.component";
+import { WaitersBookComponent } from "./menu/waiters-book/waiters-book.component";
+import { ErrorMessageComponenet } from "./material/errorMessage/errorMessage.component";
+import { ConfirmationDialog } from "./material/confirmationDailog/confirmationDialog.component";
+import { StatisticsComponent } from "./menu/statistics/statistics.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { Auth } from "./auth/auth.service";
+import { AuthInterceptor } from "./auth.interceptor";
+import { SettingsComponent } from "./settings/settings.component";
+import { NavComponent } from "./nav/nav.component";
+import { authReducer } from "./auth/store/auth.reducer";
+import { UserAreaComponent } from "./user-area/user-area.component";
 
 @NgModule({
   declarations: [
@@ -66,10 +66,19 @@ import { UserAreaComponent } from './user-area/user-area.component';
     MatTableModule,
     MatRippleModule,
     MatProgressBarModule,
-    StoreModule.forRoot({auth:authReducer})
+    StoreModule.forRoot({ auth: authReducer }),
+    
   ],
-  entryComponents: [ErrorMessageComponenet, ConfirmationDialog, RegisterComponent, SignInComponent],
-  providers: [Auth, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },],
+  entryComponents: [
+    ErrorMessageComponenet,
+    ConfirmationDialog,
+    RegisterComponent,
+    SignInComponent
+  ],
+  providers: [
+    Auth,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
